8c6f365e4f396bd45926b22786121a0c76194905
iKursus <ikursus.com@gmail.com>
2022-07-05 16:29:23 +0800

    Update query

--- resources/views/users/borang.blade.php
+++ resources/views/users/borang.blade.php
@@ -1,12 +1,12 @@
 
     <div class="mb-3">
         <label class="form-label">Nama</label>
-        <input type="text" class="form-control" name="name">
+        <input type="text" class="form-control" name="name" value="{{ $user->name ?? old('name') }}">
     </div>
 
     <div class="mb-3">
         <label class="form-label">Email</label>
-        <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+        <input type="email" class="form-control" name="email" value="{{ $user->email ?? old('email') }}">
     </div>
 
     <div class="mb-3">
@@ -16,22 +16,22 @@
 
     <div class="mb-3">
         <label class="form-label">Telefon</label>
-        <input type="text" class="form-control" name="phone">
+        <input type="text" class="form-control" name="phone" value="{{ $user->phone ?? old('phone') }}">
     </div>
 
     <div class="mb-3">
         <label class="form-label">Role</label>
         <select name="role" class="form-control">
-            <option value="USER">User</option>
-            <option value="ADMIN">Admin</option>
+            <option value="USER"{{ $user->role == 'USER' ? ' selected="selected' : null }}>User</option>
+            <option value="ADMIN"{{ $user->role == 'ADMIN' ? ' selected="selected' : null }}>Admin</option>
         </select>
     </div>
 
     <div class="mb-3">
         <label class="form-label">Status</label>
         <select name="status" class="form-control">
-            <option value="active">Active</option>
-            <option value="inactive">Inactive</option>
+            <option value="ACTIVE"{{ $user->status == 'ACTIVE' ? ' selected="selected' : null }}>Active</option>
+            <option value="INACTIVE"{{ $user->status == 'INACTIVE' ? ' selected="selected' : null }}>Inactive</option>
         </select>
     </div>
 
